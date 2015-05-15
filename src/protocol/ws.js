1f94354430c411f452a901d7714695707893bad2
Chase Miller <chmille4@gmail.com>
2015-05-14 21:38:25 -0600

    fix dependency with binary.js

--- src/protocol/ws.js
+++ src/protocol/ws.js
@@ -1,5 +1,6 @@
 var ws = function(url,opts,callback) {
 	var wsUrl = 'ws://' + url
+  var BinaryClient = require('Binary').BinaryClient;
 	var client = BinaryClient(wsUrl);                
     client.on('open', function(stream){
       var stream = client.createStream({event:'run', params : {'url':wsUrl}});          
