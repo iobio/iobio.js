f2bbf9aa1b6242f69597a938744d9af96642d006
Chase Miller <chmille4@gmail.com>
2015-05-15 23:02:23 -0600

    add back all tests

--- test/command.js
+++ test/command.js
@@ -24,34 +24,34 @@ describe("Command", function() {
         });
     });
 
-    // describe("Execute", function() {
-    //     var data;
-    //     cmd.run();
-    //     beforeEach(function(done) {
-    //         cmd.on('data', function(d) {                
-    //             data = d.split('\t')[0];
-    //             done();
-    //         },20)
-    //     });
-    //     it("simple command", function() {
-    //         expect(data).toEqual('ERR194147.602999777');                        
-    //     });
-    // });
+    describe("Execute", function() {
+        var data;
+        cmd.run();
+        beforeEach(function(done) {
+            cmd.on('data', function(d) {                
+                data = d.split('\t')[0];
+                done();
+            },20)
+        });
+        it("simple command", function() {
+            expect(data).toEqual('ERR194147.602999777');                        
+        });
+    });
 
-    // describe("Execute", function() {
-    //     var data;
-    //     var cmdPipe = new iobio.cmd('samtools.iobio.io', ['view', '-b', '-h', 'http://s3.amazonaws.com/iobio/jasmine_files/test.bam', '1'], {'encoding':'binary'})
-    //             .pipe( 'bamtools.iobio.io', ['convert', '-format', 'json'] );
-    //     cmdPipe.run();
-    //     beforeEach(function(done) {
-    //         cmdPipe.on('data', function(d) {                
-    //             data = JSON.parse(d).name;
-    //             done();
-    //         },20)
-    //     });
-    //     it("piped command", function() {
-    //         expect(data).toEqual('ERR194147.602999777');                        
-    //     });
-    // });
+    describe("Execute", function() {
+        var data;
+        var cmdPipe = new iobio.cmd('samtools.iobio.io', ['view', '-b', '-h', 'http://s3.amazonaws.com/iobio/jasmine_files/test.bam', '1'], {'encoding':'binary'})
+                .pipe( 'bamtools.iobio.io', ['convert', '-format', 'json'] );
+        cmdPipe.run();
+        beforeEach(function(done) {
+            cmdPipe.on('data', function(d) {                
+                data = JSON.parse(d).name;
+                done();
+            },20)
+        });
+        it("piped command", function() {
+            expect(data).toEqual('ERR194147.602999777');                        
+        });
+    });
 });
 
