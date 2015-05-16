cf3fcc1cefd5c6c440edae08f1cb59d105fff3bd
Chase Miller <chmille4@gmail.com>
2015-05-16 17:06:58 -0600

    update js files

--- iobio.js
+++ iobio.js
@@ -4052,7 +4052,7 @@ var cmdBuilder = function(service, params, opts) {
 		if(params[i].slice(0,8) == 'iobio://') {
 			sourceType = 'url'
 			params[i] = urlParamer(params[i]);			
-		} else if (Object.prototype.toString.call(params[i]) == '[object File]') {
+		} else if (Object.prototype.toString.call(params[i]) == '[object File]' || Object.prototype.toString.call(params[i]) == '[object Blob]') {
 			sourceType = 'file';
 			if (opts && opts.writeStream) 
 				params[i] = fileParamer(service, params[i], function (s) {me.emit('writeStream', s)}, {write:false});
@@ -4130,38 +4130,40 @@ module.exports = ws;
 // create url for file and setup streams
 
 var file = function(service, fileObj, callback, opts) {   
-   var me = this;
-   var connectionID = require('shortid').generate();
-   var extend = require('extend');
-   var options = { write:true };
-   extend(options, opts);
+    var me = this,
+        BinaryClient = require('Binary').BinaryClient,
+        connectionID = require('shortid').generate(),
+        extend = require('extend'),
+        options = { write:true };
+    
+    extend(options, opts);
 
 
 
-   // set client id for service that will be written to
-   var client = BinaryClient( 'ws://' + service + '?id=', {'connectionID' : connectionID} );
-   client.on('open', function(stream){
-      var stream = client.createStream({event:'setID', 'connectionID':connectionID});
-      stream.end();
-   })
+    // set client id for service that will be written to
+    var client = BinaryClient( 'ws://' + service + '?id=', {'connectionID' : connectionID} );
+    client.on('open', function(stream){
+        var stream = client.createStream({event:'setID', 'connectionID':connectionID});
+        stream.end();
+    })
 
-   // fires when stream is ready write
-   client.on('stream', function(stream, opts) {      
-      callback(stream);
-      if (options.write) {
-         var reader = new FileReader();               
-         reader.onload = function(evt) { stream.write(evt.target.result); }
-         reader.onloadend = function(evt) { stream.end(); }             
-         reader.readAsBinaryString(fileObj);
-      }
-   })
+    // fires when stream is ready write
+    client.on('stream', function(stream, opts) {      
+        callback(stream);
+        if (options.write) {
+            var reader = new FileReader();               
+            reader.onload = function(evt) { stream.write(evt.target.result); }
+            reader.onloadend = function(evt) { stream.end(); }             
+            reader.readAsBinaryString(fileObj);
+        }
+    })
 
-   return  encodeURIComponent("http://client?&id="+connectionID) ;
+    return  encodeURIComponent("http://client?&id="+connectionID) ;
 }
 
 
 module.exports = file;
-},{"extend":8,"shortid":13}],23:[function(require,module,exports){
+},{"Binary":2,"extend":8,"shortid":13}],23:[function(require,module,exports){
 
 var url = function(param) {	
 	var p = 'http' + param.slice(5,param.length);
