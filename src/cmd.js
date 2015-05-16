9039cc5755bed900e4df4f20bf93572e6981f28c
Chase Miller <chmille4@gmail.com>
2015-05-15 22:52:30 -0600

    change back to using global in cmd.js

--- src/cmd.js
+++ src/cmd.js
@@ -1,7 +1,7 @@
 // Grab an existing iobio namespace object, or create a blank object
 // if it doesn't exist
-// var iobio = global.iobio || {};
-// global.iobio = iobio;
+var iobio = global.iobio || {};
+global.iobio = iobio;
 var iobio = {};
 // export if being used as a node module - needed for test framework
 if ( typeof module === 'object' ) { module.exports = iobio;}
