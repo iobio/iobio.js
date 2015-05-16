10c0f7418f7675c7fd1f19d83bff41b13555dbb8
Chase Miller <chmille4@gmail.com>
2015-05-16 17:06:00 -0600

    include binary client

--- src/source/file.js
+++ src/source/file.js
@@ -1,33 +1,35 @@
 // create url for file and setup streams
 
 var file = function(service, fileObj, callback, opts) {   
-   var me = this;
-   var connectionID = require('shortid').generate();
-   var extend = require('extend');
-   var options = { write:true };
-   extend(options, opts);
+    var me = this,
+        BinaryClient = require('Binary').BinaryClient,
+        connectionID = require('shortid').generate(),
+        extend = require('extend'),
+        options = { write:true };
+    
+    extend(options, opts);
 
 
 
-   // set client id for service that will be written to
-   var client = BinaryClient( 'ws://' + service + '?id=', {'connectionID' : connectionID} );
-   client.on('open', function(stream){
-      var stream = client.createStream({event:'setID', 'connectionID':connectionID});
-      stream.end();
-   })
+    // set client id for service that will be written to
+    var client = BinaryClient( 'ws://' + service + '?id=', {'connectionID' : connectionID} );
+    client.on('open', function(stream){
+        var stream = client.createStream({event:'setID', 'connectionID':connectionID});
+        stream.end();
+    })
 
-   // fires when stream is ready write
-   client.on('stream', function(stream, opts) {      
-      callback(stream);
-      if (options.write) {
-         var reader = new FileReader();               
-         reader.onload = function(evt) { stream.write(evt.target.result); }
-         reader.onloadend = function(evt) { stream.end(); }             
-         reader.readAsBinaryString(fileObj);
-      }
-   })
+    // fires when stream is ready write
+    client.on('stream', function(stream, opts) {      
+        callback(stream);
+        if (options.write) {
+            var reader = new FileReader();               
+            reader.onload = function(evt) { stream.write(evt.target.result); }
+            reader.onloadend = function(evt) { stream.end(); }             
+            reader.readAsBinaryString(fileObj);
+        }
+    })
 
-   return  encodeURIComponent("http://client?&id="+connectionID) ;
+    return  encodeURIComponent("http://client?&id="+connectionID) ;
 }
 
 
