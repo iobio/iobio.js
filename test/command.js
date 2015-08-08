e5dc89e10835ab7daf8ee46f794288bd0938c015
Chase Miller <chmille4@gmail.com>
2015-08-08 09:56:33 -0600

    trying to fix tests on travisci

--- test/command.js
+++ test/command.js
@@ -5,20 +5,24 @@ var iobio = require('../src/cmd.js')
 describe("Command", function() {
 
     describe("Execute", function() {
-        var data;        
-        beforeEach(function(done) {   
-            var cmd = new iobio.cmd('samtools.iobio.io', ['view', 'http://s3.amazonaws.com/iobio/jasmine_files/test.bam', '1']);
-            cmd.on('error', function(error) { /*ignore errors*/ })         
-            cmd.on('data', function(d) {                                
-                data = d.split('\t')[0];                
+        var data;                                
+        beforeEach(function(done) {
+            var sam = "@HD\tVN:1.3\tSO:coordinate\n@SQ\tSN:1\tLN:249250621\n@RG\tID:ERR194147\tLB:NA12878_1\tPL:ILLUMINA\tPU:ILLUMINA-1\tSM:NA12878\nERR194147.602999777 147 1   11919   0   101M    =   11675   -345\   ATTTGCTGTCTCTTAGCCCAGACTTCCCGTGTCCTTTCCACCGGGCCTTTGAGAGGTCACAGGGTCTTGATGCTGTGGTCTTCATCTGCAGGTGTCTGACT   B@>CEIIIJJJJGHJIGGIIGDIEHFFCFHFGHIFFHEFCE@BBEBECDFDDDDBEDGEFEABEDEBDCDDEFEDDADCDBCEDCDDDECBDEDEECB?AA   MC:Z:101M   BD:Z:KBKOSRLQNONMLMPPKOOKONLLMJLLIINMNLBLMNIMLLJNNMKAJLJJJLMMMHHNLIMMKKJLMLNONHHMMMKKKMMLKNNNOMNIJONRPONJJ  MD:Z:101    PG:Z:MarkDuplicates RG:Z:ERR194147  BI:Z:OFMQTTNRPRPQOQRRMQRORQONPLNPLLPPOOFNPPLPNNLPQOOFMPLNKONOPKKPOKNOMNLOOOPQQKKPNOMNMPPOMQPPPOOLLPOSRQQMM  NM:i:0  MQ:i:0  AS:i:101    XS:i:101\n";
+            var file = new Blob([sam]);         
+
+            var cmdFile = new iobio.cmd('0.0.0.0:8060', ['view', '-S', '-H', file]);
+            cmdFile.on('error', function(error) { /*ignore error*/ })
+            cmdFile.on('data', function(d) {                
+                data = d.split("\t").join("").split("\n").join("");
                 done();
-            })            
-            cmd.run();
+            })
+            cmdFile.run();
         },20000);
-        it("simple url command", function() {
-            expect(data).toEqual('ERR194147.602999777');                        
+        it("simple file command", function() {            
+            expect(data).toEqual("@HDVN:1.3SO:coordinate@SQSN:1LN:249250621@RGID:ERR194147LB:NA12878_1PL:ILLUMINAPU:ILLUMINA-1SM:NA12878");
         });
     });
+
 
     describe("Execute", function() {
         var data;                
@@ -35,7 +39,7 @@ describe("Command", function() {
                 done();
             })
             cmdFile2.run(); 
-        },20000);
+        },5000);
         
         it("sends file data via writestream to file command", function() {            
             expect(data).toEqual("@HDVN:1.3SO:coordinate@SQSN:1LN:249250621@RGID:ERR194147LB:NA12878_1PL:ILLUMINAPU:ILLUMINA-1SM:NA12878");
@@ -60,24 +64,6 @@ describe("Command", function() {
         });
     });
 
-    describe("Execute", function() {
-        var data;                                
-        beforeEach(function(done) {
-            var sam = "@HD\tVN:1.3\tSO:coordinate\n@SQ\tSN:1\tLN:249250621\n@RG\tID:ERR194147\tLB:NA12878_1\tPL:ILLUMINA\tPU:ILLUMINA-1\tSM:NA12878\nERR194147.602999777 147 1   11919   0   101M    =   11675   -345\   ATTTGCTGTCTCTTAGCCCAGACTTCCCGTGTCCTTTCCACCGGGCCTTTGAGAGGTCACAGGGTCTTGATGCTGTGGTCTTCATCTGCAGGTGTCTGACT   B@>CEIIIJJJJGHJIGGIIGDIEHFFCFHFGHIFFHEFCE@BBEBECDFDDDDBEDGEFEABEDEBDCDDEFEDDADCDBCEDCDDDECBDEDEECB?AA   MC:Z:101M   BD:Z:KBKOSRLQNONMLMPPKOOKONLLMJLLIINMNLBLMNIMLLJNNMKAJLJJJLMMMHHNLIMMKKJLMLNONHHMMMKKKMMLKNNNOMNIJONRPONJJ  MD:Z:101    PG:Z:MarkDuplicates RG:Z:ERR194147  BI:Z:OFMQTTNRPRPQOQRRMQRORQONPLNPLLPPOOFNPPLPNNLPQOOFMPLNKONOPKKPOKNOMNLOOOPQQKKPNOMNMPPOMQPPPOOLLPOSRQQMM  NM:i:0  MQ:i:0  AS:i:101    XS:i:101\n";
-            var file = new Blob([sam]);         
-
-            var cmdFile = new iobio.cmd('0.0.0.0:8060', ['view', '-S', '-H', file]);
-            cmdFile.on('error', function(error) { /*ignore error*/ })
-            cmdFile.on('data', function(d) {                
-                data = d.split("\t").join("").split("\n").join("");
-                done();
-            })
-            cmdFile.run();
-        },20000);
-        it("simple file command", function() {            
-            expect(data).toEqual("@HDVN:1.3SO:coordinate@SQSN:1LN:249250621@RGID:ERR194147LB:NA12878_1PL:ILLUMINAPU:ILLUMINA-1SM:NA12878");
-        });
-    });
 
     describe("Stream event", function() {
         var response;        
@@ -94,6 +80,22 @@ describe("Command", function() {
             expect(response).toEqual('end');                        
         });
     });
+
+    describe("Execute", function() {
+        var data;        
+        beforeEach(function(done) {   
+            var cmd = new iobio.cmd('samtools.iobio.io', ['view', 'http://s3.amazonaws.com/iobio/jasmine_files/test.bam', '1']);
+            cmd.on('error', function(error) { /*ignore errors*/ })         
+            cmd.on('data', function(d) {                                
+                data = d.split('\t')[0];                
+                done();
+            })            
+            cmd.run();
+        },20000);
+        it("simple url command", function() {
+            expect(data).toEqual('ERR194147.602999777');                        
+        });
+    });    
 
     describe("Execute", function() {
         var data;                
